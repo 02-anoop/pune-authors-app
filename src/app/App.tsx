8ae9e2074ae11dda0e33bd540f7dc020d844520b
Anoop Saini <anoopsaini.0905@gmail.com>
2026-07-20 15:17:12 +0530

    Implement Code Splitting for Performance

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Toaster } from "sonner";
+import { lazy, Suspense } from "react";
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
-import { LandingPage } from "./components/LandingPage";
-import { AuthorLandingPage } from "./components/AuthorLandingPage";
-import { CataloguePage } from "./components/CataloguePage";
-import { CheckoutPage } from "./components/CheckoutPage";
-import { BulkCheckoutPage } from "./components/BulkCheckoutPage";
-import { AuthorRegistrationPage } from "./components/AuthorRegistrationPage";
-import { AuthorOnboardingWizard } from "./components/AuthorOnboardingWizard";
-import { AuthorDashboardPage } from "./components/AuthorDashboardPage";
-import { OperationsDashboardPage } from "./components/OperationsDashboardPage";
-import { GoaCafePage } from "./components/GoaCafePage";
-import { AboutPage } from "./components/AboutPage";
-import { ServicesPage } from "./components/ServicesPage";
-import { AuthPage } from "./components/AuthPage";
-import { CustomerProfilePage } from "./components/CustomerProfilePage";
-import { EventsPage } from "./components/EventsPage";
-import { EventCataloguePage } from "./components/EventCataloguePage";
-import { EventLogPage } from "./components/EventLogPage";
-import { ContactPage } from "./components/ContactPage";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { BookDetailPage } from "./components/BookDetailPage";
-import { FlybrariesPage } from "./components/FlybrariesPage";
-import { OrganizeEventPage } from "./components/OrganizeEventPage";
-import { OrganizerLandingPage } from "./components/OrganizerLandingPage";
-import { BrowseAuthorsPage } from "./components/BrowseAuthorsPage";
-import { AuthorPublicProfilePage } from "./components/AuthorPublicProfilePage";
+import { useLocation } from "react-router";
+
+// Lazy-loaded Pages
+const LandingPage = lazy(() => import("./components/LandingPage").then(module => ({ default: module.LandingPage })));
+const AuthorLandingPage = lazy(() => import("./components/AuthorLandingPage").then(module => ({ default: module.AuthorLandingPage })));
+const CataloguePage = lazy(() => import("./components/CataloguePage").then(module => ({ default: module.CataloguePage })));
+const CheckoutPage = lazy(() => import("./components/CheckoutPage").then(module => ({ default: module.CheckoutPage })));
+const BulkCheckoutPage = lazy(() => import("./components/BulkCheckoutPage").then(module => ({ default: module.BulkCheckoutPage })));
+const AuthorRegistrationPage = lazy(() => import("./components/AuthorRegistrationPage").then(module => ({ default: module.AuthorRegistrationPage })));
+const AuthorOnboardingWizard = lazy(() => import("./components/AuthorOnboardingWizard").then(module => ({ default: module.AuthorOnboardingWizard })));
+const AuthorDashboardPage = lazy(() => import("./components/AuthorDashboardPage").then(module => ({ default: module.AuthorDashboardPage })));
+const OperationsDashboardPage = lazy(() => import("./components/OperationsDashboardPage").then(module => ({ default: module.OperationsDashboardPage })));
+const GoaCafePage = lazy(() => import("./components/GoaCafePage").then(module => ({ default: module.GoaCafePage })));
+const AboutPage = lazy(() => import("./components/AboutPage").then(module => ({ default: module.AboutPage })));
+const ServicesPage = lazy(() => import("./components/ServicesPage").then(module => ({ default: module.ServicesPage })));
+const AuthPage = lazy(() => import("./components/AuthPage").then(module => ({ default: module.AuthPage })));
+const CustomerProfilePage = lazy(() => import("./components/CustomerProfilePage").then(module => ({ default: module.CustomerProfilePage })));
+const EventsPage = lazy(() => import("./components/EventsPage").then(module => ({ default: module.EventsPage })));
+const EventCataloguePage = lazy(() => import("./components/EventCataloguePage").then(module => ({ default: module.EventCataloguePage })));
+const EventLogPage = lazy(() => import("./components/EventLogPage").then(module => ({ default: module.EventLogPage })));
+const ContactPage = lazy(() => import("./components/ContactPage").then(module => ({ default: module.ContactPage })));
+const BookDetailPage = lazy(() => import("./components/BookDetailPage").then(module => ({ default: module.BookDetailPage })));
+const FlybrariesPage = lazy(() => import("./components/FlybrariesPage").then(module => ({ default: module.FlybrariesPage })));
+const OrganizeEventPage = lazy(() => import("./components/OrganizeEventPage").then(module => ({ default: module.OrganizeEventPage })));
+const OrganizerLandingPage = lazy(() => import("./components/OrganizerLandingPage").then(module => ({ default: module.OrganizerLandingPage })));
+const BrowseAuthorsPage = lazy(() => import("./components/BrowseAuthorsPage").then(module => ({ default: module.BrowseAuthorsPage })));
+const AuthorPublicProfilePage = lazy(() => import("./components/AuthorPublicProfilePage").then(module => ({ default: module.AuthorPublicProfilePage })));
 
 {/* MARKER-MAKE-KIT-INVOKED */}
 
-import { useLocation } from "react-router";
+// A simple loading spinner to show during chunk fetching
+const PageLoader = () => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', color: '#666' }}>
+    Loading page...
+  </div>
+);
 
 function AppContent() {
   const location = useLocation();
@@ -40,33 +49,35 @@ function AppContent() {
     <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", fontFamily: "var(--font-body)" }}>
       {!isDashboard && <NavBar />}
       <div style={{ flex: 1 }}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/authors" element={<AuthorLandingPage />} />
-          <Route path="/authors/organize-event" element={<OrganizeEventPage />} />
-          <Route path="/organizers" element={<OrganizerLandingPage />} />
-          <Route path="/organizers/organize-event" element={<OrganizeEventPage />} />
-          <Route path="/invite-authors" element={<BrowseAuthorsPage />} />
-          <Route path="/invite-authors/:id" element={<AuthorPublicProfilePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/catalogue" element={<CataloguePage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/bulk-checkout" element={<BulkCheckoutPage />} />
-          <Route path="/register" element={<AuthorOnboardingWizard />} />
-          <Route path="/login" element={<AuthPage type="login" />} />
-          <Route path="/signup" element={<AuthPage type="signup" />} />
-          <Route path="/dashboard/*" element={<AuthorDashboardPage />} />
-          <Route path="/operations/*" element={<OperationsDashboardPage />} />
-          <Route path="/profile" element={<CustomerProfilePage />} />
-          <Route path="/book-cafe" element={<GoaCafePage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/flybraries" element={<FlybrariesPage />} />
-          <Route path="/events/:eventId/catalogue" element={<EventCataloguePage />} />
-          <Route path="/gallery" element={<EventLogPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/book/:id" element={<BookDetailPage />} />
-        </Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/authors" element={<AuthorLandingPage />} />
+            <Route path="/authors/organize-event" element={<OrganizeEventPage />} />
+            <Route path="/organizers" element={<OrganizerLandingPage />} />
+            <Route path="/organizers/organize-event" element={<OrganizeEventPage />} />
+            <Route path="/invite-authors" element={<BrowseAuthorsPage />} />
+            <Route path="/invite-authors/:id" element={<AuthorPublicProfilePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/catalogue" element={<CataloguePage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/bulk-checkout" element={<BulkCheckoutPage />} />
+            <Route path="/register" element={<AuthorOnboardingWizard />} />
+            <Route path="/login" element={<AuthPage type="login" />} />
+            <Route path="/signup" element={<AuthPage type="signup" />} />
+            <Route path="/dashboard/*" element={<AuthorDashboardPage />} />
+            <Route path="/operations/*" element={<OperationsDashboardPage />} />
+            <Route path="/profile" element={<CustomerProfilePage />} />
+            <Route path="/book-cafe" element={<GoaCafePage />} />
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/flybraries" element={<FlybrariesPage />} />
+            <Route path="/events/:eventId/catalogue" element={<EventCataloguePage />} />
+            <Route path="/gallery" element={<EventLogPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/book/:id" element={<BookDetailPage />} />
+          </Routes>
+        </Suspense>
       </div>
       {!isDashboard && <Footer />}
     </div>
@@ -82,4 +93,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
